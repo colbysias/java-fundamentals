a82b0fe36b2669253fb2433a79bd0b66eecee5cb
csias <colbysias@yahoo.com>
2022-10-17 16:21:53 -0500

    Started Labs for Heirarachy Directory

---
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Heirarchy.java
@@ -0,0 +1,19 @@
+package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
+
+public class Heirarchy {
+
+}
+
+
+     class Computer{
+
+    }
+
+     class Motherboard extends Computer{
+
+    }
+
+     class Processor extends Motherboard{
+
+    }
+
