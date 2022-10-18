463e598af6441e1f7e96174615c6bc208cf2f6ef
csias <colbysias@yahoo.com>
2022-10-18 16:23:32 -0500

    Added Constructor classes for Exercise1 in OOP Directory

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Heirarchy.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Heirarchy.java
@@ -6,14 +6,26 @@ public class Heirarchy {
 
 
      class Computer{
+        String model;
+        int year;
 
-    }
+         public Computer(String model, int year) {
+             this.model = model;
+             this.year = year;
+         }
+     }
 
      class Motherboard extends Computer{
 
-    }
+        Motherboard(String model, int year) {
+            super(model, year);
+        }
+        }
 
      class Processor extends Motherboard{
 
-    }
+         Processor(String model, int year) {
+             super(model, year);
+         }
+     }
 
