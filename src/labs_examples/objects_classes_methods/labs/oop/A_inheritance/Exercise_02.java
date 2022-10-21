c459b48c8faff05e5f028b26a54d23b0befde7f0
csias <colbysias@yahoo.com>
2022-10-21 13:05:17 -0500

    Finished Exercise 1 and 2 in OOP Directory

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -4,7 +4,9 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
  * Run the class below and study the output. What prints and why?
  *
  * Your answer:
- *
+ * The int 10 is Printed because in the main method you are creating a new variable "a" from class A,
+ * even though you're extending it from B. So whenever you print out that variable "i" from the class it prints it from class A
+ * since that is the initial class we are creating the object from.
  */
 class A {
     int i = 10;
