0dda7efd63a1a3f7c8e832a75298499da0f6e1d3
colbysias <colbysias@yahoo.com>
2022-10-23 20:19:54 -0500

    Completed Inheritance Labs

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,23 @@
-class Main {
-    public static void main(String[] args) {
-
-        // please write a custom constructor in the Person class
-        // as directed below (in the Person class)
-
-        Person person1 = new Person("Colby", 30);
-        Person person2 = new Person("Devin", 26);
-        // now use that new constructor to create two unique Person objects
-
-        // now print the instance var values of each object
-        System.out.println(person1.name + " " + person1.age);
-        System.out.println(person2.name + " " + person2.age);
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Parent c = new Child();
     }
 }
 
-class Person {
+class Parent
+{
+    public Parent()
+    {
+        System.err.println("Parent called");
+    }
+}
 
-    String name;
-    int age;
-        Person(String name1, int age1){
-            name = name1;
-            age = age1;
-        }
-    // please create a fully qualified constructor
-    // (which is a constructor that takes in "name" and "age")
-    // and set the instance variables above from within the constructor
-
+class Child extends Parent
+{
+    public Child()
+    {
+        System.err.println("Child called");
+    }
 }
--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
@@ -4,7 +4,9 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
  * Why does the output print in the order it does?
  *
  * You answer:
- *
+* Since Class C_1 is a subclass of Class B_1 and class B_1 is a subclass of Class A_1, it has to start at the Parent
+ * class first and work it's way through the child classes. So to make a call to Class C_1, you have to go through
+ * it's parent classes first.
  */
 
 class A_1 {
